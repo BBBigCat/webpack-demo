bea5a9f1560cc0fad90cb8b9fc20efd224c39e82
bin.liu <bin.liu@yuanzhanghu.com>
2021-08-24 14:44:22 +0800

    chore: 简化 remote 地址

--- app2/src/Button.js
+++ app2/src/Button.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Button = () => <button>App 2 Button</button>;
+const Button = () => (
+    <button
+        onClick={() => {
+            console.log('this is App 2 Button');
+        }}
+    >
+        App 2 Button
+    </button>
+);
 
 export default Button;
